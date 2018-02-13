efc510b692ed47e565d009efe67dfa7b75ef3364
Rowen Wichtmann <rowenwichtmann@gmail.com>
2018-02-13 10:01:22 +0000

    Made some changes on my own still no luck

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts, only: [:create] # Can also use without [] when there is only one i.e. "only: :create"
+  resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
 end
